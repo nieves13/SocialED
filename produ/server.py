e1cb2369ed6fa7b6156ec8ce89caacd37b989bab
Nieves Aracil Cerdan <nievesaracil@gmail.com>
2020-05-14 15:45:53 +0200

    html1

--- produ/server.py
+++ produ/server.py
@@ -6,22 +6,22 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/static', methods=['GET'])
 def index():
     return app.send_static_file('index.html')
 
 
-@app.route('/home', methods=['GET'])
+@app.route('/static', methods=['GET'])
 def home():
     return app.send_static_file('home.html')
 
 
-@app.route('/login', methods=['GET'])
+@app.route('/static', methods=['GET'])
 def login():
     return app.send_static_file('login.html')
 
 
-@app.route('/signup', methods=['GET'])
+@app.route('/static', methods=['GET'])
 def signup():
     return app.send_static_file('signup.html')
 
